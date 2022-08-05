a1ca1baed79159d66117d56ee67d1c5071ecf2f7
Carlos Sánchez <oceanrdn@gmail.com>
2022-08-05 11:23:27 +0200

    Favicon + netlify fix

--- packages/react-app/src/hooks/index.js
+++ packages/react-app/src/hooks/index.js
@@ -1,4 +1,3 @@
 export { default as useDebounce } from "./useDebounce";
 export { default as useLocalStorage } from "./useLocalStorage";
 export { default as useStaticJsonRPC } from "./useStaticJsonRPC";
-export * from "./useContractConfig";
