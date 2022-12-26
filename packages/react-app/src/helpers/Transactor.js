5932d9b591d21ce8a0dbf6655922c1ab4e24c8d1
Carlos Sánchez <oceanrdn@gmail.com>
2022-12-26 12:37:36 +0100

    Tx error msg fix

--- packages/react-app/src/helpers/Transactor.js
+++ packages/react-app/src/helpers/Transactor.js
@@ -124,8 +124,8 @@ export default function Transactor(providerOrSigner, gasPrice, etherscan) {
             : e.data
             ? e.data
             : JSON.stringify(e);
-        if (!e.error && e.message && !e.data.message) {
-          message = e.message;
+        if (!e.error && e.message && !e.data?.message) {
+          message = e?.message;
         }
 
         console.log("Attempt to clean up:", message);
