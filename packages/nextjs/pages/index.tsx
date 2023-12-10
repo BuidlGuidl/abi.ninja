409227ff812fd3a95bda8f6a568ff588e33209b5
port <108868128+portdeveloper@users.noreply.github.com>
2023-12-10 14:11:52 +0300

    Implement the index page layout

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,58 +1,135 @@
+import { useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { HeartIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
+import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
 
 const Home: NextPage = () => {
+  const [activeTab, setActiveTab] = useState("verifiedContract");
+  const [network, setNetwork] = useState("mainnet");
+
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
+      <div className="flex flex-grow items-center justify-center bg-base-100">
+        <div className="flex h-screen w-full flex-col items-center justify-center rounded-2xl bg-white p-2 lg:h-[650px] lg:w-[450px] lg:justify-between lg:shadow-xl">
+          <div className="mt-10 flex w-7/12 flex-col items-center justify-center lg:w-10/12">
+            <Image src="/logo_inv.svg" alt="logo" width={128} height={128} className="mb-4" />
+            <h2 className="mb-0 text-5xl font-bold">ABI Ninja</h2>
+            <p className="">Interact with any contract on Ethereum</p>
+            <div className="my-4">
+              <select
+                className="select select-sm w-36 max-w-xs bg-slate-50"
+                value={network}
+                onChange={e => setNetwork(e.target.value)}
+              >
+                <option value="localhost">Localhost</option>
+                <option value="mainnet">Mainnet</option>
+                <option value="optimism">Optimism</option>
+              </select>
+            </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
+            <div role="tablist" className="tabs tabs-bordered w-full">
+              <a
+                role="tab"
+                className={`tab ${activeTab === "verifiedContract" ? "tab-active" : ""}`}
+                onClick={() => setActiveTab("verifiedContract")}
+              >
+                Verified Contract
+              </a>
+              <a
+                role="tab"
+                className={`tab ${activeTab === "addressAbi" ? "tab-active" : ""}`}
+                onClick={() => setActiveTab("addressAbi")}
+              >
+                Address + ABI
+              </a>
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+
+            <div className="min-h-[150px] w-full">
+              {activeTab === "verifiedContract" && (
+                <div className="my-4">
+                  <input
+                    type="text"
+                    placeholder="Verified contract address"
+                    className="input h-9 w-full bg-slate-100"
+                  />
+                  <div className="flex flex-col text-sm">
+                    <div className="mb-2 mt-4 text-center font-semibold">Quick Access</div>
+                    <div className="flex justify-around">
+                      <Link
+                        href="/placeholder"
+                        passHref
+                        className="link w-1/3 text-center text-purple-700 no-underline"
+                      >
+                        DAI
+                      </Link>
+                      <Link
+                        href="/placeholder"
+                        passHref
+                        className="link w-1/3 text-center text-purple-700 no-underline"
+                      >
+                        Gitcoin
+                      </Link>
+                      <Link
+                        href="/placeholder"
+                        passHref
+                        className="link w-1/3 text-center text-purple-700 no-underline"
+                      >
+                        Opensea
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              {activeTab === "addressAbi" && (
+                <div className="my-4 flex w-full flex-col gap-3">
+                  <input type="text" placeholder="Contract address" className="input h-9 w-full bg-slate-100" />
+                  <input placeholder="Contract ABI(json format)" className="input h-9 w-full bg-slate-100" />
+                </div>
+              )}
             </div>
+            <button className="btn btn-primary w-1/2">Load Contract</button>
+          </div>
+          <div className="mt-10">
+            <ul className="menu menu-horizontal w-full">
+              <div className="flex w-full items-center justify-center gap-2 text-xs">
+                <div className="text-center">
+                  <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer" className="link">
+                    Fork me
+                  </a>
+                </div>
+                <span>·</span>
+                <div className="flex items-center justify-center gap-2">
+                  <p className="m-0 text-center">
+                    Built with <HeartIcon className="inline-block h-4 w-4" /> at
+                  </p>
+                  <a
+                    className="flex items-center justify-center gap-1"
+                    href="https://buidlguidl.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <BuidlGuidlLogo className="h-5 w-3 pb-1" />
+                    <span className="link">BuidlGuidl</span>
+                  </a>
+                </div>
+                <span>·</span>
+                <div className="text-center">
+                  <a
+                    href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="link"
+                  >
+                    Support
+                  </a>
+                </div>
+              </div>
+            </ul>
           </div>
         </div>
       </div>
