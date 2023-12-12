ffaa816f4d93087da424b56a57ceacd526aa317c
port <108868128+portdeveloper@users.noreply.github.com>
2023-12-12 18:45:13 +0300

    Fix tab styling in home page

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -90,7 +90,7 @@ const Home: NextPage = () => {
             <div role="tablist" className="flex w-full border-b">
               <a
                 role="tab"
-                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 ${
+                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 hover:cursor-pointer${
                   activeTab === "verifiedContract"
                     ? "border-purple-500 "
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
@@ -101,7 +101,7 @@ const Home: NextPage = () => {
               </a>
               <a
                 role="tab"
-                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 ${
+                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 hover:cursor-pointer${
                   activeTab === "addressAbi"
                     ? "border-purple-500 "
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
