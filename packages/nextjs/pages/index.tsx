2ffca74b12338352d192059e3fc9e053b04c795e
port <108868128+portdeveloper@users.noreply.github.com>
2023-12-12 19:35:19 +0300

    Fix tab border color in index page

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -90,9 +90,9 @@ const Home: NextPage = () => {
             <div role="tablist" className="flex w-full border-b">
               <a
                 role="tab"
-                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 hover:cursor-pointer${
+                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 hover:cursor-pointer ${
                   activeTab === "verifiedContract"
-                    ? "border-purple-500 "
+                    ? "border-purple-500"
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
                 }`}
                 onClick={() => setActiveTab("verifiedContract")}
@@ -101,9 +101,9 @@ const Home: NextPage = () => {
               </a>
               <a
                 role="tab"
-                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 hover:cursor-pointer${
+                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 hover:cursor-pointer ${
                   activeTab === "addressAbi"
-                    ? "border-purple-500 "
+                    ? "border-purple-500"
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
                 }`}
                 onClick={() => setActiveTab("addressAbi")}
