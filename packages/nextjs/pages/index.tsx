f469baef8873f4ffc72fe422a833163bf4cdf271
port <108868128+portdeveloper@users.noreply.github.com>
2023-12-12 12:18:40 +0300

    Fix tabs in macOS? by using raw tailwind

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -64,17 +64,25 @@ const Home: NextPage = () => {
               </select>
             </div>
 
-            <div role="tablist" className="tabs tabs-bordered w-full">
+            <div role="tablist" className="flex w-full border-b">
               <a
                 role="tab"
-                className={`tab ${activeTab === "verifiedContract" ? "tab-active" : ""}`}
+                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 ${
+                  activeTab === "verifiedContract"
+                    ? "border-purple-500 "
+                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                }`}
                 onClick={() => setActiveTab("verifiedContract")}
               >
                 Verified Contract
               </a>
               <a
                 role="tab"
-                className={`tab ${activeTab === "addressAbi" ? "tab-active" : ""}`}
+                className={`inline-block px-4 py-2 text-sm w-full font-medium text-center border-b-2 ${
+                  activeTab === "addressAbi"
+                    ? "border-purple-500 "
+                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                }`}
                 onClick={() => setActiveTab("addressAbi")}
               >
                 Address + ABI
