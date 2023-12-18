bb0d66aa3fcd15114bc77cd26e9cd5ec2a5063e9
port <108868128+portdeveloper@users.noreply.github.com>
2023-12-18 18:44:10 +0300

    Extract footer to its own component

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -3,9 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import type { NextPage } from "next";
-import { HeartIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
-import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
+import { MiniFooter } from "~~/components/MiniFooter";
 
 const Home: NextPage = () => {
   const [activeTab, setActiveTab] = useState("verifiedContract");
@@ -167,43 +166,7 @@ const Home: NextPage = () => {
               Load Contract
             </button>
           </div>
-          <div className="mt-10">
-            <ul className="menu menu-horizontal w-full">
-              <div className="flex w-full items-center justify-center gap-2 text-xs">
-                <div className="text-center">
-                  <a href="https://github.com/scaffold-eth/se-2" target="_blank" rel="noreferrer" className="link">
-                    Fork me
-                  </a>
-                </div>
-                <span>·</span>
-                <div className="flex items-center justify-center gap-2">
-                  <p className="m-0 text-center">
-                    Built with <HeartIcon className="inline-block h-4 w-4" /> at
-                  </p>
-                  <a
-                    className="flex items-center justify-center gap-1"
-                    href="https://buidlguidl.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <BuidlGuidlLogo className="h-5 w-3 pb-1" />
-                    <span className="link">BuidlGuidl</span>
-                  </a>
-                </div>
-                <span>·</span>
-                <div className="text-center">
-                  <a
-                    href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="link"
-                  >
-                    Support
-                  </a>
-                </div>
-              </div>
-            </ul>
-          </div>
+          <MiniFooter />
         </div>
       </div>
     </>
