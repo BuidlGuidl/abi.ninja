99536e711fe4210c0300daa2e068de9d138fefe7
port <108868128+portdeveloper@users.noreply.github.com>
2023-12-29 12:42:44 +0300

    Add contractabi and address to zustand store

--- packages/nextjs/services/store/store.ts
+++ packages/nextjs/services/store/store.ts
@@ -1,28 +1,22 @@
+import { Abi, Address } from "viem";
 import create from "zustand";
 import scaffoldConfig from "~~/scaffold.config";
 import { ChainWithAttributes } from "~~/utils/scaffold-eth";
-
-/**
- * Zustand Store
- *
- * You can add global state to the app using this useGlobalState, to get & set
- * values from anywhere in the app.
- *
- * Think about it as a global useState.
- */
 
 type GlobalState = {
   nativeCurrencyPrice: number;
   setNativeCurrencyPrice: (newNativeCurrencyPriceState: number) => void;
   targetNetwork: ChainWithAttributes;
   setTargetNetwork: (newTargetNetwork: ChainWithAttributes) => void;
-  contractAbi: string;
-  setContractAbi: (newAbi: string) => void;
 };
 
 type AbiNinjaState = {
   mainChainId: number;
   setMainChainId: (newMainChainId: number) => void;
+  contractAbi: Abi;
+  setContractAbi: (newAbi: Abi) => void;
+  abiContractAddress: Address;
+  setAbiContractAddress: (newAbiContractAddress: Address) => void;
 };
 
 export const useGlobalState = create<GlobalState>(set => ({
@@ -30,11 +24,13 @@ export const useGlobalState = create<GlobalState>(set => ({
   setNativeCurrencyPrice: (newValue: number): void => set(() => ({ nativeCurrencyPrice: newValue })),
   targetNetwork: scaffoldConfig.targetNetworks[0],
   setTargetNetwork: (newTargetNetwork: ChainWithAttributes) => set(() => ({ targetNetwork: newTargetNetwork })),
-  contractAbi: "", // default value for contract ABI
-  setContractAbi: (newAbi: string): void => set({ contractAbi: newAbi }),
 }));
 
 export const useAbiNinjaState = create<AbiNinjaState>(set => ({
   mainChainId: 1,
   setMainChainId: (newValue: number): void => set(() => ({ mainChainId: newValue })),
+  contractAbi: [],
+  setContractAbi: (newAbi: Abi): void => set({ contractAbi: newAbi }),
+  abiContractAddress: "",
+  setAbiContractAddress: (newAddress: Address): void => set({ abiContractAddress: newAddress }),
 }));
