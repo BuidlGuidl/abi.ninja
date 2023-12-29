07be158c220d1a53eb5168602de109173a4c736e
Pablo Alayeto <55535804+Pabl0cks@users.noreply.github.com>
2023-12-29 23:52:18 +0100

    show spinner while loading contractData

--- packages/nextjs/pages/[contractAddress]/[network].tsx
+++ packages/nextjs/pages/[contractAddress]/[network].tsx
@@ -4,6 +4,7 @@ import { ParsedUrlQuery } from "querystring";
 import { Abi, isAddress } from "viem";
 import { Footer } from "~~/components/Footer";
 import { MiniHeader } from "~~/components/MiniHeader";
+import { Spinner } from "~~/components/assets/Spinner";
 import { ContractUI } from "~~/components/scaffold-eth";
 import { useAbiNinjaState } from "~~/services/store/store";
 import { fetchContractABIFromEtherscan } from "~~/utils/abi";
@@ -22,6 +23,7 @@ const ContractDetailPage = () => {
   const router = useRouter();
   const { contractAddress, network } = router.query as ParsedQueryContractDetailsPage;
   const [contractData, setContractData] = useState<ContractData>({ abi: [], address: contractAddress });
+  const [isLoading, setIsLoading] = useState(true);
   const contractName = contractData.address;
   const setMainChainId = useAbiNinjaState(state => state.setMainChainId);
 
@@ -36,8 +38,10 @@ const ContractDetailPage = () => {
       try {
         const abi = await fetchContractABIFromEtherscan(contractAddress, parseInt(network));
         setContractData({ abi: JSON.parse(abi), address: contractAddress });
+        setIsLoading(false);
       } catch (e) {
         console.error("Error while getting abi: ", e);
+        setIsLoading(false);
       }
     };
 
@@ -50,7 +54,9 @@ const ContractDetailPage = () => {
     <div className="bg-base-100 min-h-screen">
       <MiniHeader />
       <div className="flex flex-col gap-y-6 lg:gap-y-8 justify-center items-center">
-        {contractData.abi?.length > 0 ? (
+        {isLoading ? (
+          <Spinner />
+        ) : contractData.abi?.length > 0 ? (
           <ContractUI key={contractName} deployedContractData={contractData} />
         ) : (
           <h1 className="text-2xl">Contract not found</h1>
