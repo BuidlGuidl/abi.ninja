ff7b345f45b16bd04a28c5c8142049db1307352f
tokodev <pablo@asdloop.com>
2024-01-01 23:58:21 +0100

    add MetaHeader to ContractDetailPage

--- packages/nextjs/pages/[contractAddress]/[network].tsx
+++ packages/nextjs/pages/[contractAddress]/[network].tsx
@@ -6,6 +6,7 @@ import { Abi, isAddress } from "viem";
 import * as chains from "viem/chains";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { Footer } from "~~/components/Footer";
+import { MetaHeader } from "~~/components/MetaHeader";
 import { MiniHeader } from "~~/components/MiniHeader";
 import { ContractUI } from "~~/components/scaffold-eth";
 import { useAbiNinjaState } from "~~/services/store/store";
@@ -72,31 +73,34 @@ const ContractDetailPage = () => {
   }, [contractAddress, network, storedAbi]);
 
   return (
-    <div className="bg-base-100 min-h-screen">
-      <MiniHeader />
-      <div className="flex flex-col gap-y-6 lg:gap-y-8 justify-center items-center">
-        {isLoading ? (
-          <span className="loading loading-spinner text-primary h-14 w-14"></span>
-        ) : contractData.abi?.length > 0 ? (
-          <ContractUI key={contractName} deployedContractData={contractData} />
-        ) : (
-          <div className="bg-white border shadow-xl rounded-2xl px-6 lg:px-8 m-4">
-            <ExclamationTriangleIcon className="text-red-500 mt-4 h-8 w-8" />
-            <h2 className="text-2xl pt-2 flex items-end">{error}</h2>
-            <p className="break-all">
-              There was an error loading the contract <strong>{contractAddress}</strong> on{" "}
-              <strong>{getNetworkName(network)}</strong>.
-            </p>
-            <p className="pb-2">Make sure the data is correct and you are connected to the right network.</p>
+    <>
+      <MetaHeader />
+      <div className="bg-base-100 min-h-screen">
+        <MiniHeader />
+        <div className="flex flex-col gap-y-6 lg:gap-y-8 justify-center items-center">
+          {isLoading ? (
+            <span className="loading loading-spinner text-primary h-14 w-14"></span>
+          ) : contractData.abi?.length > 0 ? (
+            <ContractUI key={contractName} deployedContractData={contractData} />
+          ) : (
+            <div className="bg-white border shadow-xl rounded-2xl px-6 lg:px-8 m-4">
+              <ExclamationTriangleIcon className="text-red-500 mt-4 h-8 w-8" />
+              <h2 className="text-2xl pt-2 flex items-end">{error}</h2>
+              <p className="break-all">
+                There was an error loading the contract <strong>{contractAddress}</strong> on{" "}
+                <strong>{getNetworkName(network)}</strong>.
+              </p>
+              <p className="pb-2">Make sure the data is correct and you are connected to the right network.</p>
 
-            <button className="btn btn-primary text-center p-2 text-base border-2 mb-4">
-              <Link href="/">Go back to homepage</Link>
-            </button>
-          </div>
-        )}
+              <button className="btn btn-primary text-center p-2 text-base border-2 mb-4">
+                <Link href="/">Go back to homepage</Link>
+              </button>
+            </div>
+          )}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 
