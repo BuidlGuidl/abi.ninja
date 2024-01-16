99663b03b36328311e96e5e9f2545ee0e541ef24
tokodev <pablo@asdloop.com>
2024-01-17 00:24:57 +0100

    tweak miniheader to work with blockexplorer bg color

--- packages/nextjs/components/MiniHeader.tsx
+++ packages/nextjs/components/MiniHeader.tsx
@@ -4,7 +4,7 @@ import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 
 export const MiniHeader = () => {
   return (
-    <div className="sticky lg:static top-0 navbar bg-white min-h-0 flex-shrink-0 justify-between z-20 px-0 sm:px-2 mb-10">
+    <div className="sticky lg:static top-0 navbar bg-white border-b border-secondary min-h-0 flex-shrink-0 justify-between z-20 px-0 sm:px-2 mb-10">
       <div className="navbar-start w-auto lg:w-1/2">
         <Link href="/" passHref className="hidden sm:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex items-center">
