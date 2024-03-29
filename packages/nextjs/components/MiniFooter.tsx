3345c3ccf21ef4f9a7529788c8a38b664211c6c9
tokodev <pablo@asdloop.com>
2024-03-30 00:15:18 +0100

    Fix footer not being clickable

--- packages/nextjs/components/MiniFooter.tsx
+++ packages/nextjs/components/MiniFooter.tsx
@@ -3,7 +3,7 @@ import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
 
 export const MiniFooter = () => {
   return (
-    <div className="flex justify-center items-center gap-1 text-xs w-full mt-10">
+    <div className="flex justify-center items-center gap-1 text-xs w-full mt-10 z-10">
       <div className="mb-1">
         <a href="https://github.com/BuidlGuidl/abi.ninja" target="_blank" rel="noreferrer" className="link">
           Fork me
