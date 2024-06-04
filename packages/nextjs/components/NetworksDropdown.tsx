d5894f88bee8c8c2221be9d1680febc0231cd32b
port <108868128+portdeveloper@users.noreply.github.com>
2024-06-04 07:41:41 +0300

    Add "see all chains" option to the dropdown

--- packages/nextjs/components/NetworksDropdown.tsx
+++ packages/nextjs/components/NetworksDropdown.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import * as chains from "@wagmi/core/chains";
 import { useTheme } from "next-themes";
-import Select, { OptionProps, components } from "react-select";
+import Select, { MultiValue, OptionProps, SingleValue, components } from "react-select";
 import { getTargetNetworks } from "~~/utils/scaffold-eth";
 
 type Options = {
-  value: number;
+  value: number | string;
   label: string;
   icon?: string;
 };
@@ -20,7 +21,6 @@ type GroupedOptions = Record<
 const networks = getTargetNetworks();
 const groupedOptions = networks.reduce<GroupedOptions>(
   (groups, network) => {
-    // Handle the case for localhost
     if (network.id === 31337) {
       groups.localhost.options.push({
         value: network.id,
@@ -47,6 +47,18 @@ const groupedOptions = networks.reduce<GroupedOptions>(
   },
 );
 
+groupedOptions.mainnet.options.push({
+  value: "see-all",
+  label: "See All Chains",
+  icon: "/mainnet.svg",
+});
+
+const allChains = Object.values(chains).map(chain => ({
+  value: chain.id,
+  label: chain.name,
+  icon: "/mainnet.svg",
+}));
+
 const { Option } = components;
 const IconOption = (props: OptionProps<Options>) => (
   <Option {...props}>
@@ -57,9 +69,11 @@ const IconOption = (props: OptionProps<Options>) => (
   </Option>
 );
 
-export const NetworksDropdown = ({ onChange }: { onChange: (options: any) => any }) => {
+export const NetworksDropdown = ({ onChange }: { onChange: (option: Options | null) => void }) => {
   const [isMobile, setIsMobile] = useState(false);
   const { resolvedTheme } = useTheme();
+  const [selectedOption, setSelectedOption] = useState<SingleValue<Options>>(groupedOptions.mainnet.options[0]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const isDarkMode = resolvedTheme === "dark";
 
@@ -80,36 +94,95 @@ export const NetworksDropdown = ({ onChange }: { onChange: (options: any) => any
     }
   }, []);
 
+  const handleSelectChange = (newValue: SingleValue<Options> | MultiValue<Options>) => {
+    const selected = newValue as SingleValue<Options>;
+    if (selected?.value === "see-all") {
+      (document.getElementById("see-all-modal") as HTMLDialogElement)?.showModal();
+    } else {
+      setSelectedOption(selected);
+      onChange(selected);
+    }
+  };
+
+  const filteredChains = allChains.filter(chain => chain.label.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  console.log({ filteredChains });
+
   if (!mounted) return null;
+
   return (
-    <Select
-      defaultValue={groupedOptions["mainnet"].options[0]}
-      instanceId="network-select"
-      options={Object.values(groupedOptions)}
-      onChange={onChange}
-      components={{ Option: IconOption }}
-      isSearchable={!isMobile}
-      className="max-w-xs relative text-sm w-44"
-      theme={theme => ({
-        ...theme,
-        colors: {
-          ...theme.colors,
-          primary25: isDarkMode ? "#401574" : "#efeaff",
-          primary50: isDarkMode ? "#551d98" : "#c1aeff",
-          primary: isDarkMode ? "#BA8DE8" : "#551d98",
-          neutral0: isDarkMode ? "#130C25" : theme.colors.neutral0,
-          neutral80: isDarkMode ? "#ffffff" : theme.colors.neutral80,
-        },
-      })}
-      styles={{
-        menuList: provided => ({ ...provided, maxHeight: 280, overflow: "auto" }),
-        control: provided => ({ ...provided, borderRadius: 12 }),
-        indicatorSeparator: provided => ({ ...provided, display: "none" }),
-        menu: provided => ({
-          ...provided,
-          border: `1px solid ${isDarkMode ? "#555555" : "#a3a3a3"}`,
-        }),
-      }}
-    />
+    <>
+      <Select
+        value={selectedOption}
+        instanceId="network-select"
+        options={Object.values(groupedOptions)}
+        onChange={handleSelectChange}
+        components={{ Option: IconOption }}
+        isSearchable={!isMobile}
+        className="max-w-xs relative text-sm w-44"
+        theme={theme => ({
+          ...theme,
+          colors: {
+            ...theme.colors,
+            primary25: isDarkMode ? "#401574" : "#efeaff",
+            primary50: isDarkMode ? "#551d98" : "#c1aeff",
+            primary: isDarkMode ? "#BA8DE8" : "#551d98",
+            neutral0: isDarkMode ? "#130C25" : theme.colors.neutral0,
+            neutral80: isDarkMode ? "#ffffff" : theme.colors.neutral80,
+          },
+        })}
+        styles={{
+          menuList: provided => ({ ...provided, maxHeight: 280, overflow: "auto" }),
+          control: provided => ({ ...provided, borderRadius: 12 }),
+          indicatorSeparator: provided => ({ ...provided, display: "none" }),
+          menu: provided => ({
+            ...provided,
+            border: `1px solid ${isDarkMode ? "#555555" : "#a3a3a3"}`,
+          }),
+        }}
+      />
+
+      <dialog id="see-all-modal" className="modal">
+        <div className="flex flex-col modal-box p-12 h-3/4 w-11/12 max-w-5xl bg-base-200">
+          <div className="flex justify-between items-center mb-6">
+            <h3 className="font-bold text-xl">All Chains</h3>
+            <div className="modal-action mt-0">
+              <button
+                className="btn btn-error"
+                onClick={() => (document.getElementById("see-all-modal") as HTMLDialogElement)?.close()}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+          <input
+            type="text"
+            placeholder="Search chains..."
+            className="input input-bordered w-full mb-4 bg-neutral"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+          />
+
+          <div className="flex flex-wrap content-start justify-center gap-4 overflow-y-auto h-3/4 p-2">
+            {filteredChains.map(option => (
+              <div
+                key={`${option.label}-${option.value}`}
+                className="card shadow-md bg-base-100 cursor-pointer h-28 w-60 text-center"
+                onClick={() => {
+                  setSelectedOption(option);
+                  onChange(option);
+                  (document.getElementById("see-all-modal") as HTMLDialogElement)?.close();
+                }}
+              >
+                <div className="card-body flex flex-col justify-center items-center p-4">
+                  <span className="text-sm font-semibold">Chain Id: {option.value}</span>
+                  <span className="text-sm">{option.label}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </dialog>
+    </>
   );
 };
