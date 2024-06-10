6da3cf46e15f029004a257ccedaaa846ac5d9ece
port <108868128+portdeveloper@users.noreply.github.com>
2024-06-10 11:01:43 +0300

    Ask the user to add the chain and/or abi if they do not exist in [networks].tsx

--- packages/nextjs/pages/[contractAddress]/[network].tsx
+++ packages/nextjs/pages/[contractAddress]/[network].tsx
@@ -3,17 +3,18 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 import { ParsedUrlQuery } from "querystring";
-import { Abi, isAddress } from "viem";
+import { Abi, createPublicClient, defineChain, http, isAddress } from "viem";
 import * as chains from "viem/chains";
-import { usePublicClient } from "wagmi";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 import { MiniHeader } from "~~/components/MiniHeader";
+import { Options } from "~~/components/NetworksDropdown";
 import { SwitchTheme } from "~~/components/SwitchTheme";
 import { ContractUI } from "~~/components/scaffold-eth";
 import { useAbiNinjaState } from "~~/services/store/store";
-import { fetchContractABIFromAnyABI, fetchContractABIFromEtherscan } from "~~/utils/abi";
+import { fetchContractABIFromAnyABI, fetchContractABIFromEtherscan, parseAndCorrectJSON } from "~~/utils/abi";
 import { detectProxyTarget } from "~~/utils/abi-ninja/proxyContracts";
+import { notification } from "~~/utils/scaffold-eth";
 
 interface ParsedQueryContractDetailsPage extends ParsedUrlQuery {
   contractAddress: string;
@@ -52,6 +53,8 @@ const ContractDetailPage = ({ addressFromUrl, chainIdFromUrl }: ServerSideProps)
   const [contractData, setContractData] = useState<ContractData>({ abi: [], address: contractAddress });
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [localContractAbi, setLocalContractAbi] = useState("");
+  const [customChains, setCustomChains] = useState<Options[]>([]);
   const contractName = contractData.address;
   const {
     contractAbi: storedAbi,
@@ -65,13 +68,54 @@ const ContractDetailPage = ({ addressFromUrl, chainIdFromUrl }: ServerSideProps)
     setImplementationAddress: state.setImplementationAddress,
   }));
 
-  const publicClient = usePublicClient({
-    chainId: parseInt(network),
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const customChains = JSON.parse(localStorage.getItem("customChains") || "[]");
+      setCustomChains(customChains);
+    }
+  }, []);
+
+  let selectedChain = Object.values(chains).find(chain => chain.id === parseInt(network));
+  if (!selectedChain) {
+    const customChain = customChains.find((chain: any) => chain.value === chainId);
+    if (customChain) {
+      // @ts-ignore
+      selectedChain = defineChain({
+        id: +customChain.value,
+        name: customChain.label,
+        nativeCurrency: {
+          decimals: 18,
+          name: customChain.label,
+          symbol: customChain.label,
+        },
+        rpcUrls: {
+          default: {
+            http: [customChain.rpcUrl as string],
+          },
+          public: {
+            http: [customChain.rpcUrl as string],
+          },
+        },
+        network: customChain.label, // shouldn't be necessary according to docs, but it is
+      });
+    } else {
+      selectedChain = Object.values(chains).find(chain => chain.id === 1);
+    }
+  }
+
+  const publicClient = createPublicClient({
+    chain: selectedChain,
+    transport: http(),
   });
 
   const getNetworkName = (chainId: number) => {
-    const chain = Object.values(chains).find(chain => chain.id === chainId);
-    return chain ? chain.name : "Unknown Network";
+    const predefinedChain = Object.values(chains).find(chain => chain.id === chainId);
+    if (predefinedChain) {
+      return predefinedChain.name;
+    }
+
+    const customChain = customChains.find(chain => chain.value === chainId);
+    return customChain ? customChain.label : "Unknown Network";
   };
 
   useEffect(() => {
@@ -103,6 +147,7 @@ const ContractDetailPage = ({ addressFromUrl, chainIdFromUrl }: ServerSideProps)
         }
 
         try {
+          // @ts-ignore
           const implementationAddress = await detectProxyTarget(contractAddress, publicClient);
 
           if (implementationAddress) {
@@ -138,7 +183,44 @@ const ContractDetailPage = ({ addressFromUrl, chainIdFromUrl }: ServerSideProps)
         }
       }
     }
-  }, [contractAddress, network, storedAbi, setMainChainId, setImplementationAddress, publicClient]);
+  }, []);
+
+  const handleUserProvidedAbi = () => {
+    try {
+      const parsedAbi = parseAndCorrectJSON(localContractAbi);
+      setContractData({ abi: parsedAbi, address: contractAddress });
+      notification.success("ABI successfully loaded.");
+    } catch (error) {
+      notification.error("Invalid ABI format. Please ensure it is a valid JSON.");
+    }
+  };
+
+  const handleAddCustomChain = (newChain: any) => {
+    const chainOption = {
+      value: newChain.id,
+      label: newChain.name,
+      icon: "PuzzlePieceIcon",
+      rpcUrl: newChain.rpcUrl,
+    };
+    const updatedChains = [...customChains, chainOption];
+    setCustomChains(updatedChains);
+    if (typeof window !== "undefined") {
+      localStorage.setItem("customChains", JSON.stringify(updatedChains));
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const newChain = {
+      id: parseInt(formData.get("id") as string),
+      name: formData.get("name") as string,
+      rpcUrl: formData.get("rpcUrl") as string,
+    };
+    handleAddCustomChain(newChain);
+    handleUserProvidedAbi();
+    e.currentTarget.reset();
+  };
 
   return (
     <>
@@ -153,16 +235,83 @@ const ContractDetailPage = ({ addressFromUrl, chainIdFromUrl }: ServerSideProps)
           ) : contractData.abi?.length > 0 ? (
             <ContractUI key={contractName} initialContractData={contractData} />
           ) : (
-            <div className="bg-base-200 border shadow-xl rounded-2xl px-6 lg:px-8 m-4">
+            <div className="bg-base-200 flex flex-col border shadow-xl rounded-2xl px-6 lg:px-8 m-4">
               <ExclamationTriangleIcon className="text-red-500 mt-4 h-8 w-8" />
               <h2 className="text-2xl pt-2 flex items-end">{error}</h2>
               <p className="break-all">
                 There was an error loading the contract <strong>{contractAddress}</strong> on{" "}
                 <strong>{getNetworkName(chainId)}</strong>.
               </p>
-              <p className="pb-2">Make sure the data is correct and you are connected to the right network.</p>
+              <p className="pb-2">
+                Make sure the data is correct and you are connected to the right network. Or add the chain/ABI below.
+              </p>
+              <div className="flex justify-center gap-12">
+                {customChains.some(chain => chain.value === chainId) ? (
+                  <div className="w-1/2">
+                    <form className="bg-base-200" onSubmit={handleSubmit}>
+                      <h3 className="font-bold text-xl">Add Custom ABI</h3>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">ABI</span>
+                        </label>
+                        <textarea
+                          className="textarea bg-neutral w-full h-full mb-4 resize-none"
+                          placeholder="Paste contract ABI in JSON format here"
+                          value={localContractAbi}
+                          onChange={e => setLocalContractAbi(e.target.value)}
+                        ></textarea>
+                      </div>
+                      <div className="modal-action mt-6">
+                        <button type="submit" className="btn btn-primary">
+                          Submit ABI
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                ) : (
+                  <div className="w-1/2">
+                    <form className="bg-base-200" onSubmit={handleSubmit}>
+                      <h3 className="font-bold text-xl">Add Custom Chain and ABI</h3>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">Chain ID</span>
+                        </label>
+                        <input type="number" name="id" className="input input-bordered bg-neutral" required />
+                      </div>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name="name" className="input input-bordered bg-neutral" required />
+                      </div>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">RPC URL</span>
+                        </label>
+                        <input type="text" name="rpcUrl" className="input input-bordered bg-neutral" required />
+                      </div>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">ABI</span>
+                        </label>
+                        <textarea
+                          className="textarea bg-neutral w-full h-full mb-4 resize-none"
+                          placeholder="Paste contract ABI in JSON format here"
+                          value={localContractAbi}
+                          onChange={e => setLocalContractAbi(e.target.value)}
+                        ></textarea>
+                      </div>
+                      <div className="modal-action mt-6">
+                        <button type="submit" className="btn btn-primary">
+                          Submit Chain and Import ABI
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                )}
+              </div>
 
-              <button className="btn btn-primary text-center p-2 text-base border-2 mb-4">
+              <button className="btn btn-ghost text-center self-end p-2 text-base border-2 mb-4">
                 <Link href="/">Go back to homepage</Link>
               </button>
             </div>
