1ddd9a0f101e22274e15972ae041fb7bfaefd575
Shiv Bhonde <shivbhonde04@gmail.com>
2024-06-29 14:14:19 +0530

    fix useTargenetwork to include all networks

--- packages/nextjs/hooks/scaffold-eth/useTargetNetwork.ts
+++ packages/nextjs/hooks/scaffold-eth/useTargetNetwork.ts
@@ -1,10 +1,10 @@
 import { useEffect, useMemo } from "react";
 import { useAccount } from "wagmi";
-import scaffoldConfig from "~~/scaffold.config";
 import { useGlobalState } from "~~/services/store/store";
-import { ChainWithAttributes } from "~~/utils/scaffold-eth";
+import { ChainWithAttributes, getTargetNetworks } from "~~/utils/scaffold-eth";
 import { NETWORKS_EXTRA_DATA } from "~~/utils/scaffold-eth";
 
+const mainNetworks = getTargetNetworks();
 /**
  * Retrieves the connected wallet's network from scaffold.config or defaults to the 0th network in the list if the wallet is not connected.
  */
@@ -14,7 +14,7 @@ export function useTargetNetwork(): { targetNetwork: ChainWithAttributes } {
   const setTargetNetwork = useGlobalState(({ setTargetNetwork }) => setTargetNetwork);
 
   useEffect(() => {
-    const newSelectedNetwork = scaffoldConfig.targetNetworks.find(targetNetwork => targetNetwork.id === chain?.id);
+    const newSelectedNetwork = mainNetworks.find(targetNetwork => targetNetwork.id === chain?.id);
     if (newSelectedNetwork && newSelectedNetwork.id !== targetNetwork.id) {
       setTargetNetwork(newSelectedNetwork);
     }
