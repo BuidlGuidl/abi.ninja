ee274f2a66449b3bb10cac2806b2b686863dcd20
port dev <108868128+portdeveloper@users.noreply.github.com>
2024-12-27 21:14:20 +0300

    Add a test for quick access

--- packages/nextjs/tests/example.spec.ts
+++ packages/nextjs/tests/example.spec.ts
@@ -1,3 +1,4 @@
+// @ts-check
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
@@ -5,4 +6,12 @@ test("has title", async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/ABI Ninja/);
+});
+
+test("can load one of the quick access contracts", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+
+  await page.getByRole("link", { name: "DAI" }).click();
+
+  await expect(page.getByRole("link", { name: "0x6B1...1d0F" })).toBeVisible();
 });
