938737631c50804b8eac93f5b30d4a8590d18f11
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-08-24 15:38:47 +0300

    Use eslint cli in precommit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,21 +1,15 @@
 const path = require("path");
 
 const buildNextEslintCommand = (filenames) =>
-  `yarn next:lint --fix --file ${filenames
+  `yarn workspace @se-2/nextjs eslint --fix ${filenames
     .map((f) => path.relative(path.join("packages", "nextjs"), f))
-    .join(" --file ")}`;
+    .join(" ")}`;
 
 const checkTypesNextCommand = () => "yarn next:check-types";
-
-const buildHardhatEslintCommand = (filenames) =>
-  `yarn hardhat:lint-staged --fix ${filenames
-    .map((f) => path.relative(path.join("packages", "hardhat"), f))
-    .join(" ")}`;
 
 module.exports = {
   "packages/nextjs/**/*.{ts,tsx}": [
     buildNextEslintCommand,
     checkTypesNextCommand,
   ],
-  "packages/hardhat/**/*.{ts,tsx}": [buildHardhatEslintCommand],
 };
