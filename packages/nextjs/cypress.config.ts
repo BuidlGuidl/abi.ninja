ff2b3c0674dd926fb0a646a80fa64c019692eb0c
Rinat <rinatkins@gmail.com>
2026-01-16 10:25:16 +0400

    fix: tests (#194)
    * fix: tests
    
    * fix: react-native-async-storage
    
    * fix: burner-connector
    
    * fix: add timeout
    
    * chore: more timeouts

--- packages/nextjs/cypress.config.ts
+++ packages/nextjs/cypress.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 15000,
   },
 });
